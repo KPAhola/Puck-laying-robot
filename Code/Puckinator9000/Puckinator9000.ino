838d6aa6d4c7b36dfcd0db49bbfc4e025f5f6276
Kari-Pekka Ahola <karipekka.ahola@protonmail.com>
2023-04-19 12:09:04 +0300

    Code: Add a variable to keep track of travelled distance and a function to separately display each digit of said variable

--- Code/Puckinator9000/Puckinator9000.ino
+++ Code/Puckinator9000/Puckinator9000.ino
@@ -3,6 +3,8 @@
 #define STOP_DISTANCE 10
 #define SOUND_WAVE_TRAVEL_TIME_TO_DISTANCE_MULTIPLIER 0.01723
 
+static double distance_travelled = 0;
+
 long readUltrasonicDistance(int triggerPin, int echoPin)
 {
   pinMode(triggerPin, OUTPUT);  // Clear the trigger
@@ -17,6 +19,20 @@ long readUltrasonicDistance(int triggerPin, int echoPin)
   return pulseIn(echoPin, HIGH);
 }
 
+void display_distance(double distance) {
+  int digit1 = distance / 1000;
+  distance -= 1000 * digit1;
+  int digit2 = distance / 100;
+  distance -= 100 * digit2;
+  int digit3 = distance / 10;
+  distance -= 10 * digit3;
+  int digit4 = distance;
+  Serial.print(digit1);
+  Serial.print(digit2);
+  Serial.print(digit3);
+  Serial.println(digit4);
+}
+
 void setup()
 {
   pinMode(A5, INPUT);
@@ -28,10 +44,10 @@ void loop()
 {
   double obstacle_distance = SOUND_WAVE_TRAVEL_TIME_TO_DISTANCE_MULTIPLIER * readUltrasonicDistance(10, 9);
   if (obstacle_distance <= STOP_DISTANCE) {
-    digitalWrite(3, 1);
+    digitalWrite(3, HIGH);
    } else {
   	analogWrite(3, map(analogRead(A5), 0, 1023, 0, 255));
    }
-  Serial.println(obstacle_distance);
+  display_distance(distance_travelled);
   delay(500); // Wait for 500 millisecond(s)
 }
